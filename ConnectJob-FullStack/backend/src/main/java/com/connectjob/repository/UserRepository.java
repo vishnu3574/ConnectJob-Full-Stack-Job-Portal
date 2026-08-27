630eb64f9bdb9967d802f2268f384fc9d917c666
vishnu3574 <vishnuroyal216@gmail.com>
2026-08-27 15:09:50 +0530

    Update UserRepository.java

--- ConnectJob-FullStack/backend/src/main/java/com/connectjob/repository/UserRepository.java
+++ ConnectJob-FullStack/backend/src/main/java/com/connectjob/repository/UserRepository.java
@@ -1,1 +1,10 @@
-package com.connectjob.repository; import com.connectjob.model.User; import org.springframework.data.jpa.repository.JpaRepository; import java.util.*; public interface UserRepository extends JpaRepository<User,Long>{ Optional<User> findByEmail(String email); boolean existsByEmail(String email); }
+package com.connectjob.repository;
+
+import com.connectjob.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
